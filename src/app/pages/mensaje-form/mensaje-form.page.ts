f0f32e9af2569eb13b064aeeddebac2615c2f353
SOFTOURSISTEMAS\r.garcia <rafa@softoursistemas.com>
2025-04-03 10:09:21 +0200

    Selección de usuarios en el mensaje

--- src/app/pages/mensaje-form/mensaje-form.page.ts
+++ src/app/pages/mensaje-form/mensaje-form.page.ts
@@ -21,13 +21,16 @@ import {
   IonTextarea,
   IonSelect,
   IonSelectOption,
+  IonList,
 } from '@ionic/angular/standalone';
 import { MensajeService } from '../../services/mensaje.service';
 import { ToastService } from '../../services/toast.service';
 import { addIcons } from 'ionicons';
-import { closeOutline } from 'ionicons/icons';
+import { closeOutline, personCircleOutline } from 'ionicons/icons';
 import { UsuarioService } from '../../services/usuario.service';
 import { StorageService } from '../../services/storage.service';
+import { Usuario } from '../../interfaces/user.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mensaje-form',
@@ -35,6 +38,7 @@ import { StorageService } from '../../services/storage.service';
   styleUrls: ['./mensaje-form.page.scss'],
   standalone: true,
   imports: [
+    IonList,
     IonText,
     IonContent,
     IonHeader,
@@ -50,12 +54,15 @@ import { StorageService } from '../../services/storage.service';
     IonTextarea,
     IonSelect,
     IonSelectOption,
+    FormsModule,
   ],
 })
 export class MensajeFormPage implements OnInit {
   mensajeForm: FormGroup;
   isEdit = false;
   users: any[] = []; // Lista de usuarios
+
+  usuariosFiltrados = [...this.users];
 
   private mensajeService = inject(MensajeService);
   private toastService = inject(ToastService);
@@ -65,13 +72,14 @@ export class MensajeFormPage implements OnInit {
   private storageService = inject(StorageService);
 
   constructor(private fb: FormBuilder) {
-    addIcons({ closeOutline });
+    addIcons({ closeOutline, personCircleOutline });
 
     this.mensajeForm = this.fb.group({
       title: ['', Validators.required],
       body: ['', Validators.required],
       audience: ['all', Validators.required],
       receiverId: [''],
+      searchTerm: [''],
     });
   }
 
@@ -90,6 +98,22 @@ export class MensajeFormPage implements OnInit {
     });
   }
 
+  filtrarUsuarios() {
+    const searchTerm =
+      this.mensajeForm.get('searchTerm')?.value?.toLowerCase() || ''; // Obtener el valor de searchTerm desde el formulario
+    this.usuariosFiltrados = this.users.filter((user) =>
+      user.name.toLowerCase().includes(searchTerm)
+    );
+  }
+
+  seleccionarUsuario(user: Usuario) {
+    this.mensajeForm.patchValue({
+      receiverId: user._id,
+      searchTerm: user.name, // Actualizar searchTerm en el formulario
+    });
+    this.usuariosFiltrados = []; // Ocultar la lista tras selección
+  }
+
   cargarMensaje(id: string) {
     this.mensajeService.getMensajeById(id).subscribe((mensaje) => {
       if (mensaje) {
@@ -103,15 +127,16 @@ export class MensajeFormPage implements OnInit {
       return;
     }
     const senderId = await this.obtenerSenderId();
-    const mensajeData = { ...this.mensajeForm.value, senderId };
+    const { searchTerm, ...mensajeData } = this.mensajeForm.value;
+    const mensaje = { ...mensajeData, senderId };
 
     if (this.isEdit) {
-      this.mensajeService.updateMensaje(mensajeData).subscribe(() => {
+      this.mensajeService.updateMensaje(mensaje).subscribe(() => {
         this.toastService.showMessage('Mensaje actualizado', 'success');
         this.router.navigate(['/mensaje']);
       });
     } else {
-      this.mensajeService.createMensaje(mensajeData).subscribe(() => {
+      this.mensajeService.createMensaje(mensaje).subscribe(() => {
         this.toastService.showMessage('Mensaje creado', 'success');
         this.router.navigate(['/mensaje']);
       });
