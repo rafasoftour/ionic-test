7553daf430d136e290a5315fd738cd6cfd84300f
SOFTOURSISTEMAS\r.garcia <rafa@softoursistemas.com>
2025-04-04 16:43:20 +0200

    Con icono y splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.softoursistemas.ionictest',
-  appName: 'ionic-test',
-  webDir: 'www'
+  appName: 'PlannerStats',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+    },
+  },
 };
 
 export default config;
